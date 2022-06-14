17de4d562462773ff83ef572125c4d680640764a
Barxhe1209 <42666634+Barxhe@users.noreply.github.com>
2022-06-14 17:30:31 +0800

    update

--- C#/Chapter09/WorkingWithStreams/Program.cs
+++ C#/Chapter09/WorkingWithStreams/Program.cs
@@ -4,6 +4,8 @@ using static System.IO.Path;
 using static System.IO.Directory;
 
 
+WorkWithXml();
+
 
 static class Viper
 {
@@ -34,5 +36,46 @@ static class Viper
         arg1: new FileInfo(textFile).Length);
 
         WriteLine(File.ReadAllText(textFile));
+    }
+
+    static void WorkWithXml()
+    {
+        //define a fiel to write to
+        string xmlFile = Combine(CurrentDirectory,"streams.xml");
+
+        //create a file stream
+        FileStream xmlFileStream = File.Create(xmlFile);
+
+        //wrap the file stream in an XML writer helper
+        //and automatically indent nested elements
+        XmlWriter xml = XmlWriter.Create(xmlFileStream,
+        new XmlWriterSettings {Indent = true});
+
+        //write the XML declaration
+        xml.WriteStartDocument();
+
+        //write a root element 
+        xml.WriteStartDocument("callsigns");
+
+        //enumerate the strings writing each one to the stream
+        foreach(string item in Viper.Callsigns)
+        {
+            xml.WriteElementString("callsign",item);
+        }
+
+        //write the close root element
+        xml.WriteEndElement();
+
+        //close helper and stream
+        xml.Close();
+        xmlFileStream.Close();
+
+        //output all the contents of the file
+        WriteLine("{0} contains {1:N0} bytes",
+        arg0:xmlFile,
+        arg1:new FileInfo(xmlFile).Length);
+        WriteLine(File.ReadAllText(xmlFile));
+
+
     }
 }
