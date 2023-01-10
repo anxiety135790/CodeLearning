c44422e53c7b69149e9d6725c644857ab9b4e921
Barxhe1209 <42666634+Barxhe@users.noreply.github.com>
2023-01-11 00:01:13 +0800

    update

---
+++ Csharp/c10&net6/Chapter12/WorkingWithTasks/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Diagnostics;
+
+using static System.Console;
+
+
+OutputThreadInfo();
+Stopwatch timer = Stopwatch.StartNew();
+
+/*
+WriteLine("Running methods sysnchronously on one thread.");
+MethodA();
+MethodB();
+MethodC();
+*/
+
+/*
+WriteLine("Runing methos asynchronously on multiple threads");
+
+Task taskA = new(MethodA);
+taskA.Start();
+
+Task taskB = Task.Factory.StartNew(MethodB);
+
+Task taskC = Task.Run(MethodC);
+
+Task[] tasks = {taskA, taskB, taskC};
+Task.WaitAll(tasks);
+*/
+
+WriteLine("Passing the result of one task as an input into another."); 
+Task<string> taskServiceThenSProc = Task.Factory
+ .StartNew(CallWebService) // returns Task<decimal>
+ .ContinueWith(previousTask => // returns Task<string>
+    CallStoredProcedure(previousTask.Result));
+WriteLine($"Result: {taskServiceThenSProc.Result}");
+
+
+WriteLine($"{timer.ElapsedMilliseconds:#, ##0}ms elapsed.");
+
+static void OutputThreadInfo()
+{
+    Thread t = Thread.CurrentThread;
+
+    WriteLine(
+        "Thread Id:{0}, Priority: {1}, Background: {2}, Name: {3}",
+        t.ManagedThreadId, t.Priority,
+        t.IsBackground, t.Name ?? "null");
+        
+}
+
+static void MethodA()
+{
+    WriteLine("Starting Method A ......");
+    OutputThreadInfo();
+    Thread.Sleep(3000);
+    WriteLine("Finished Method A .");
+
+}
+
+
+
+static void MethodB()
+{
+    WriteLine("Starting Method B ......");
+    OutputThreadInfo();
+    Thread.Sleep(3000);
+    WriteLine("Finished Method B .");
+
+}
+
+
+static void MethodC()
+{
+    WriteLine("Starting Method C ......");
+    OutputThreadInfo();
+    Thread.Sleep(3000);
+    WriteLine("Finished Method C .");
+
+}
+
+static decimal CallWebService()
+{
+    WriteLine("");
+    OutputThreadInfo();
+    Thread.Sleep((new Random()).Next(2000,4000));
+    WriteLine("Finished call to web service. ");
+    return 89.99M;
+}
+
+static string CallStoredProcedure(decimal amount)
+{
+    WriteLine("");
+    OutputThreadInfo();
+    Thread.Sleep((new Random()).Next(2000,4000));
+    WriteLine(
+        "Finished call to stored Procedure.");
+    return $"12 products cost more than {amount:C}";  
+}
