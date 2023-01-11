e5caa649491798b3ddaa897b3732ba5ccdf69dba
Barxhe1209 <42666634+Barxhe@users.noreply.github.com>
2023-01-11 22:36:34 +0800

    update

---
+++ Csharp/c10&net6/Chapter12/NestedAndChildTasks/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+
+Task outerTask = Task.Factory.StartNew(OuterMethod);
+outerTask.Wait();
+WriteLine("Console app is stopping.");
+
+static void OuterMethod()
+{
+    WriteLine("Outer method startig ...");
+    Task innerTask = Task.Factory.StartNew(innerMethod,TaskCreationOptions.AttachedToParent);
+    WriteLine("Outer method stoping.");
+}
+
+static void innerMethod()
+{
+    WriteLine("Inner method starting ...");
+    Thread.Sleep(2000);
+    WriteLine("Inner method stoping.");
+}
